f5be83604c3168416d949128c1aef795b60963ba
Asif <msasifiiuc@gmail.com>
2023-09-25 22:53:22 +0600

    search filter of home page properly working

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,40 @@
-import "./Search.css"
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Search.css';
 
-const Search = () => {
+const Search = ({ handleSearchSubmit=""}) => {
+    const [searchText, setSearchText] = useState(''); 
+    const handleInputChange = (e) => {
+        setSearchText(e.target.value);
+    };
+
+    // Handler function to handle the form submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleSearchSubmit(searchText); 
+    };
+
     return (
         <div className="mt-32">
             <h1 className="text-center text-5xl font-bold text-purple-700">I Grow By Helping People In Need</h1>
-            <div className="pt-6 pb-20 flex justify-center mx-auto">
-                <input className="p-2 rounded-l-xl  w-[300px] bg-slate-200" type="text" placeholder="Search here..." />
-                <button className="py-2 px-4 bg-[#FF444A] rounded-r-xl text-white">Search</button>
-            </div>
+            <form onSubmit={handleSubmit} className="pt-6 pb-20 flex justify-center mx-auto">
+                <input
+                    className="p-2 rounded-l-xl w-[300px] bg-slate-200"
+                    type="text"
+                    placeholder="Search category here..."
+                    value={searchText}
+                    onChange={handleInputChange}
+                    name="search"
+                />
+                <button type="submit" className="py-2 px-4 bg-[#FF444A] rounded-r-xl text-white">
+                    Search
+                </button>
+            </form>
         </div>
     );
 };
 
+Search.propTypes = {
+    handleSearchSubmit: PropTypes.func.isRequired,
+};
 export default Search;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,38 @@
-
 import { useEffect, useState } from "react";
 import CardDonation from "../../components/CardDonation/CardDonation";
 import Search from "../../components/Search/Search";
 
-
 const Home = () => {
-    
     const [donations, setDonations] = useState([]);
+    const [displayDonations, setDisplayDonations] = useState([]);
+
+    const handleSearchSubmit = (searchText) => {
+        if (searchText) {
+            const matchedDonations = donations.filter(donation =>
+                donation.category.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setDisplayDonations(matchedDonations);
+        } else {
+            setDisplayDonations(donations);
+        }
+    };
+
     useEffect(() => {
         fetch('/donation.json')
             .then(res => res.json())
-            .then(data => setDonations(data))
-    }, [])
+            .then(data => {
+                setDonations(data);
+                setDisplayDonations(data);
+            });
+    }, []);
+
     return (
         <div className="md:container mx-auto">
-            <Search></Search>
-            <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                {
-                    donations?.map(donation => <CardDonation key={donation.id} donation={donation}></CardDonation>)
-                }
+            <Search handleSearchSubmit={handleSearchSubmit}></Search>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {displayDonations.map(donation => (
+                    <CardDonation key={donation.id} donation={donation} handleSearchSubmit={handleSearchSubmit} />
+                ))}
             </div>
         </div>
     );
